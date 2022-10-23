9eecc10d450d9197f3ac8a4beacb747554807ca0
alcarreon <alcarreon20@gmail.com>
2022-10-23 06:06:06 -0400

    leetcode challenge for 10_24

---
+++ easy/leetcode10_23.js
@@ -0,0 +1,29 @@
+// Length of Last Word
+
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+const lengthOfLastWord = (str) => {
+  return str.trim().split(" ").slice(-1)[0].length;
+};
+
+console.log(lengthOfLastWord("Hello World")); // 5
+console.log(lengthOfLastWord("   fly me   to   the moon  ")); // 4
+console.log(lengthOfLastWord("luffy is still joyboy")); // 6
