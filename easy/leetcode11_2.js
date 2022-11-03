743e5688713d67917addc3f8a920ffa2971b2bf5
alcarreon <alcarreon20@gmail.com>
2022-11-02 21:23:00 -0400

    changes

--- easy/leetcode11_2.js
+++ easy/leetcode11_2.js
@@ -46,8 +46,8 @@ const plusOne = (arr) => {
   return arr;
 };
 
-console.log(plusOne([1, 2, 3])); // [1,2,4]
+// console.log(plusOne([1, 2, 3])); // [1,2,4]
 // console.log(plusOne([4, 3, 2, 1])); // [4,3,2,2]
-console.log(plusOne([9])); // [1,0]
-console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
-console.log(plusOne([9, 9])); // [1,0,0]
+// console.log(plusOne([9])); // [1,0]
+// console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+// console.log(plusOne([9, 9])); // [1,0,0]
